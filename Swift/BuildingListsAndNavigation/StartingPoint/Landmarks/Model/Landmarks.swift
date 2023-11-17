e16c7e730873dc3120c809c877cbdc9d6f81c715
Mike <minp4545@gmail.com>
2023-11-16 23:00:46 -0500

    Swift update

---
+++ Swift/BuildingListsAndNavigation/StartingPoint/Landmarks/Model/Landmarks.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  Landmarks
+//
+//  Created by Min Pan on 11/5/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+
+struct Landmarks: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+
+
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+
+
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ Swift/BuildingListsAndNavigation/StartingPoint/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,19 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Min Pan on 11/5/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
